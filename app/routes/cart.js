b46908cfc74a04f9539178adcadc489cec112102
marllonsc <marllonsc@gmail.com>
2022-09-06 11:50:50 -0300

    class 03 - Router and Controller

---
+++ app/routes/cart.js
@@ -0,0 +1,16 @@
+import Route from '@ember/routing/route';
+
+export default class CartRoute extends Route {
+  model() {
+    const itens = [{ price: 10 }, { price: 15 }];
+    return itens;
+  }
+
+ /* setupController(controller, model){
+    super.setupController(controller, model);
+    const subtotal = model.reduce((acc, item) => {
+      return acc + item.price;
+    }, 0);
+    controller.set('subtotal', subtotal);
+  }*/
+}
